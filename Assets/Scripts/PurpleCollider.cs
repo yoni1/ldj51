a3e2f2014f245c2c9449e25f363e178b2afe00f4
garlicalien@gmail.com <geran@google.com>
2022-10-01 21:38:21 +0300

    Add zilla mouth

--- Assets/Scripts/PurpleCollider.cs
+++ Assets/Scripts/PurpleCollider.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrangeCollider : MonoBehaviour
+public class PurpleCollider : MonoBehaviour
 {
-    public GameObject currentVCamera;
-    public GameObject nextVCamera;
+    public ZillaBrain zillaBrain;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            currentVCamera.SetActive(false);
-            nextVCamera.SetActive(true);
+            zillaBrain.Chomp();
         }
     }
 }
