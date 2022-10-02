beb936198c83d5c5911ecf4757908d10af86fcad
garlicalien@gmail.com <geran@google.com>
2022-10-02 17:26:12 +0300

    Reappear player when camera finishes moving

---
+++ Assets/CameraBlendHandler.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraBlendHandler : MonoBehaviour
+{
+    public GameObject player;
+
+    private CinemachineBrain brain;
+    private bool currentlyBlending;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brain = GetComponent<CinemachineBrain>();
+        currentlyBlending = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!currentlyBlending && brain.IsBlending)
+        {
+            currentlyBlending = true;
+            player.SetActive(false);
+        }
+        else if (currentlyBlending && !brain.IsBlending)
+        {
+            currentlyBlending = false;
+            player.SetActive(true);
+        }
+    }
+}
