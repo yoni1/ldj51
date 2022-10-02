e80ca71fb16e4d9ea2da2b0094918bbdb7225ee9
Yoni <yoni@guardicore.com>
2022-10-02 14:47:56 +0300

    Items may be picked up and it will show that in the HUD

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public ItemManager ItemManager;
+    public string ItemTag;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            ItemManager.OnPickedUp(ItemTag);
+        }
+    }
+}
