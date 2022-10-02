b39fae116fafa9adb586f486dc536520f28c2e4f
Alon Reznik <alon@rivery.io>
2022-10-03 00:10:40 +0300

    shoorting properly

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,4 +13,11 @@ public class Bullet : MonoBehaviour
     {
         rb.velocity = transform.right * bulletSpeed * -1;
     }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        // Destroy(gameObject);
+    }
 }
+    
