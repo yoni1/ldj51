889f6b2cf2b14f0582b7eb1bb6d65eac5d294bf1
Yoni <yoni@guardicore.com>
2022-10-04 01:18:43 +0300

    Video subtitles attempt

--- Assets/Scripts/IntroCutsceneController.cs
+++ Assets/Scripts/IntroCutsceneController.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class IntroCutsceneController : MonoBehaviour
 {
-    public AudioSource dialog;
-    
-    void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (Input.GetKeyDown("space") || !dialog.isPlaying)
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.url = System.IO.Path.Combine(Application.streamingAssetsPath, "OpeningCutscene.mp4");
+        player.loopPointReached += EndReached;
+        player.Play();
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene("Building");
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        NextScene();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("Building");
+            NextScene();
         }
     }
 }
