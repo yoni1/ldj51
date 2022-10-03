612ef3c8a48c578f1aa8a0f81141dbbc5d9738a3
sabanahman <114876329+sabanahman@users.noreply.github.com>
2022-10-04 01:24:41 +0300

    Fixed Credits scene

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-     public float scrollSpeed = 0.5F;
+     public float scrollSpeed = 0.8F;
+     public float stopY = 0.0F;
      public Renderer rend;
 
     // Start is called before the first frame update
@@ -14,12 +15,10 @@ public class Credits : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        float offset = Time.time * scrollSpeed - 8;
-        //rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));        
-        if (offset < 0){
-            transform.position = new Vector3(0, offset, 0);
-        }
+        float offset = Time.deltaTime * scrollSpeed;
+        if (transform.position.y <= stopY)
+            transform.position += new Vector3(0, offset, 0);
     }
 }
